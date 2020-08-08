dc4cad7b1bbda14515115a455865eee6bf94a17f
diferunah <dfmolina@unah.hn>
2020-08-07 18:51:19 -0600

    agregando tarea 8

--- Ejercicios/Tarea 8/tarea8.cpp
+++ Ejercicios/Tarea 8/tarea8.cpp
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
     resultado = (sqrt (x));
 
     cout << endl;
-    cout << "La raiz cudrada de " << x << " es: " << resultado;
+    cout << "La raiz cuadrada de " << x << " es: " << resultado;
 
     return 0;
 }
